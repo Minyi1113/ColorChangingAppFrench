90784ff4fabdefd098d682bc0d8902bbbaa9a816
minyi <tug22058@temple.edu>
2020-10-07 21:07:31 -0400

    Finish to create the APK

--- app/src/main/java/edu/temple/colorchangingapp/CanvasActivity.java
+++ app/src/main/java/edu/temple/colorchangingapp/CanvasActivity.java
@@ -5,6 +5,7 @@ import android.app.AppComponentFactory;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import androidx.constraintlayout.widget.ConstraintLayout;
@@ -29,50 +30,62 @@ public class CanvasActivity extends AppCompatActivity {
             gridlayout.setBackgroundColor(Color.RED);
             gridtext.setText("RED");
             gridlayout.addView(gridtext);
+            gridlayout.setPadding(450,850,400,100);
         }else if(color == 1){
             gridlayout.setBackgroundColor(Color.YELLOW);
             gridtext.setText("YELLOW");
             gridlayout.addView(gridtext);
+            gridlayout.setPadding(450,850,450,100);
         } else if(color == 2){
             gridlayout.setBackgroundColor(Color.GREEN);
             gridtext.setText("GREEN");
             gridlayout.addView(gridtext);
+            gridlayout.setPadding(450,850,450,100);
         }else if(color == 3){
             gridlayout.setBackgroundColor(Color.LTGRAY);
             gridtext.setText("LIGHTGREY");
             gridlayout.addView(gridtext);
+            gridlayout.setPadding(450,850,400,100);
         }else if(color == 4){
             gridlayout.setBackgroundColor(Color.BLUE);
             gridtext.setText("BLUE");
             gridlayout.addView(gridtext);
+            gridlayout.setPadding(450,850,450,100);
         }else if(color == 5){
             gridlayout.setBackgroundColor(Color.GRAY);
             gridtext.setText("GRAY");
             gridlayout.addView(gridtext);
+            gridlayout.setPadding(450,850,450,100);
         }else if(color == 6){
             gridlayout.setBackgroundColor(Color.WHITE);
             gridtext.setText("WHITE");
             gridlayout.addView(gridtext);
+            gridlayout.setPadding(450,850,450,100);
         }else if(color == 7){
             gridlayout.setBackgroundColor(Color.BLACK);
             gridtext.setText("BLACK");
             gridlayout.addView(gridtext);
+            gridlayout.setPadding(450,850,450,100);
         }else if(color == 8){
             gridlayout.setBackgroundColor(Color.CYAN);
             gridtext.setText("CYAN");
             gridlayout.addView(gridtext);
+            gridlayout.setPadding(450,850,450,100);
         }else if(color == 9){
             gridlayout.setBackgroundColor(Color.DKGRAY);
             gridtext.setText("DKGRAY");
             gridlayout.addView(gridtext);
+            gridlayout.setPadding(450,850,450,100);
         }else if(color == 10){
             gridlayout.setBackgroundColor(Color.MAGENTA);
             gridtext.setText("MAGENTA");
             gridlayout.addView(gridtext);
+            gridlayout.setPadding(450,850,400,100);
         }else if(color == 11){
             gridlayout.setBackgroundColor(Color.RED);
             gridtext.setText("RED");
             gridlayout.addView(gridtext);
+            gridlayout.setPadding(450,850,450,100);
         }
     }
 }
